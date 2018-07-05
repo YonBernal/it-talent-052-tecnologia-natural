cdbf5e3f9fd453c889bfdf7019289f1afac1882b
yonbernal <javierortegonmp@gmail.com>
2018-07-04 21:20:49 -0500

    Ejercicio de get y servicio api rest

--- proyecto/sistema/app/controllers/libro_controller.rb
+++ proyecto/sistema/app/controllers/libro_controller.rb
@@ -1,9 +1,51 @@
 class LibroController < ApplicationController
+	protect_from_forgery unless: -> {request.format.json?}
 
 def book
 
 	@libros=Libro.all
 
+end
+
+def get_libros
+
+libros=Libro.all
+response = []
+libros.each do |b|
+	response.push({autor: b.autor.id})
+end	
+render json: {
+	libros: response
+
+}
+end
+
+def get_autores
+
+autores=Autor.all
+response=[]
+autores.each do|c|
+	response.push({
+		nombre: c.nombre,
+		apellidos: c.apellidos,
+		edad: c.edad
+	})
+
+	end
+
+	render json: {
+	autores: response}
+
+end
+
+def vista_async
+end
+
+def create
+	b=eval(request.body.read)
+	render json: {
+		estatus: 'ok'
+	}
 end
 
 end
--- proyecto/sistema/config/routes.rb
+++ proyecto/sistema/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'libro/book'
+get 'libro/get_libros'
+get 'libro/get_autores'
+get 'libro/vista_async'
+post 'libro/create'
 
 
 end
